0ad600212c60b2560b7ff46eb41f8b3c71ff7aff
Henedio Neto <netopedrosa13@gmail.com>
2023-09-11 20:37:19 -0300

    Manipulando tabela do excell com pandas e envio automatico de email

---
+++ main.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import win32com.client as win32
+
+# Importar base de dados
+tabela_vendas = pd.read_excel('Vendas.xlsx')
+
+# Visualizar base de dados
+pd.set_option('display.max_columns', None)
+
+# Faturamento por loja
+faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+
+# Quantidade de produtos vendidos por loja
+quantidade = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+
+# Ticket médio por produto em cada loja
+ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
+ticket_medio = ticket_medio.rename(columns={0: 'Ticket Médio'})
+
+# Enviar um email com relatório
+outlook = win32.Dispatch('outlook.application')
+mail = outlook.CreateItem(0)
+mail.To = 'netopedrosa13@gmail.com'
+mail.Subject = 'Relatório de Vendas'
+mail.HTMLBody = f'''
+<p>Segue o relatório de vendas</p>
+
+<p>Faturamento</p>
+{faturamento.to_html(formatters={'Valor Final': 'R$:{:,.2f}'.format})}
+
+<p>Quantidade vendida</p>
+{quantidade.to_html()}
+
+<p>Ticket Médio dos produtos em cada loja</p>
+{ticket_medio.to_html(formatters={'Ticket Médio': 'R$:{:,.2f}'.format})}
+
+<p>Att.,</p>
+<p>Henédio</p>
+'''
+
+mail.Send()
